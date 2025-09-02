e5a9845be18610e518301cd0ea5b92eaab966191
maturar <35850677+maturar@users.noreply.github.com>
2025-09-02 13:27:44 +0000

    An example for threads

--- cpp-hello-world/src/main.cpp
+++ cpp-hello-world/src/main.cpp
@@ -1,85 +1,62 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Function to be run by the thread
+// void func() {
+//     cout << "Hello from the thread!" << endl;
+// }
+
+// int main() {
+//     cout << "Main thread started." << endl;
+    
+//     // Create a thread that runs 
+//     // the function func
+//     thread t(func);
+    
+//     // Main thread waits for 't' to finish
+//     t.join();  
+//     cout << "Main thread finished." << endl;
+//     return 0;
+// }
+
+// -------------------------------------------
+
 #include <iostream>
-using namespace std;
+#include <thread>
+#include <chrono>
+using namespace std; 
 
-// §12
-// Special member functions
-//  . §12.1     Constructor
-//  . §12.8     Copy/Move
-//    - §12/1   Copy/Move Constructor
-//    - §13.5.3 Copy/Move Assignment Operator
-struct T {
-    int _v1;
-    int _v2;
-    int _v3;
+void task1() {
+    cout << "Thread 1 is running. ID: " << this_thread::get_id() << "\n";
+}
 
-    friend std::ostream& operator<<(std::ostream& os, const T& p)
-    {
-        return os << "[ " << p._v1 << " | " << p._v2 << " | " << p._v3 << " ]";
-    }
-};
+void task2() {
+    cout << "Thread 2 is running. ID: " << this_thread::get_id() << "\n";
+}
 
-struct S {
-    S() // Constructor
-    {
-        cout << "Constructing instance of S" << endl;
-        _t = new T{ 1, 2, 3 };
-    }
-    S(T& t) // Constructor
-    {
-        cout << "Initializing instance of S" << endl;
-        _t = new T{ t };
+int main() {
+    thread t1(task1);
+    thread t2(task2);
+
+    // Get thread IDs
+    cout << "t1 ID: " << t1.get_id() << "\n";
+    cout << "t2 ID: " << t2.get_id() << "\n";
+
+    // Join t1 if joinable
+    if (t1.joinable()) {
+        t1.join();
+        cout << "t1 joined\n";
     }
 
-    S(const S& that) // Copy Constructor
-    {
-        cout << "Copying instance of S" << endl;
-        _t = new T;
-        *_t = *(that._t); // Deep copy
-    }
-    S& operator=(const S& that) // Copy Assignment Operator
-    {
-        cout << "Assigning instance of S" << endl;
-        *_t = *(that._t); // Deep copy
-        return *this;
+    // Detach t2
+    if (t2.joinable()) {
+        t2.detach();
+        cout << "t2 detached\n";
     }
 
-    S(S&& that) // Move Constructor
-    {
-        cout << "Moving instance of S" << endl;
-        _t = that._t; // Move resources
-        that._t = nullptr; // Reset source (protect)
-    }
-    S& operator=(S&& that) // Move Assignment Operator
-    {
-        cout << "Move-assigning instance of S" << endl;
-        _t = that._t; // Move resources
-        that._t = nullptr; // Reset source (protect)
-        return *this;
-    }
-
-    T* _t;
-};
-
-int main()
-{
-    T t1{ 41, 42, 43 };
-    cout << t1 << " Initializer" << endl;
-    S s1{ t1 };
-    cout << s1._t << " : " << *(s1._t) << " Initialized" << endl;
-
-    S s2{ s1 };
-    cout << s2._t << " : " << *(s2._t) << " Copy Constructed" << endl;
-
-    S s3;
-    cout << s3._t << " : " << *(s3._t) << " Default Constructed" << endl;
-    s3 = s2;
-    cout << s3._t << " : " << *(s3._t) << " Copy Assigned" << endl;
-
-    S s4{ static_cast<S&&>(s3) };
-    cout << s4._t << " : " << *(s4._t) << " Move Constructed" << endl;
-
-    s2 = std::move(s4);
-    cout << s2._t << " : " << *(s2._t) << " Move Assigned" << endl;
+    cout << "Main thread sleeping for 1 second...\n";
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "Main thread awake.\n";
 
     return 0;
 }
