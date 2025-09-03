6b9d68a54145c53c7089cd3a4bcc6a7512491e84
maturar <35850677+maturar@users.noreply.github.com>
2025-09-03 15:10:34 +0000

    Refactor main.cpp to demonstrate constexpr usage with Fibonacci calculation

--- cpp-hello-world/src/main.cpp
+++ cpp-hello-world/src/main.cpp
@@ -1,62 +1,15 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+// A C++ program to demonstrate the use of constexpr 
+#include<iostream>  
 
-// // Function to be run by the thread
-// void func() {
-//     cout << "Hello from the thread!" << endl;
-// }
+constexpr long int fib(int n) 
+{ 
+    return (n <= 1) ? n : fib(n-1) + fib(n-2); 
+} 
 
-// int main() {
-//     cout << "Main thread started." << endl;
-    
-//     // Create a thread that runs 
-//     // the function func
-//     thread t(func);
-    
-//     // Main thread waits for 't' to finish
-//     t.join();  
-//     cout << "Main thread finished." << endl;
-//     return 0;
-// }
-
-// -------------------------------------------
-
-#include <iostream>
-#include <thread>
-#include <chrono>
-using namespace std; 
-
-void task1() {
-    cout << "Thread 1 is running. ID: " << this_thread::get_id() << "\n";
-}
-
-void task2() {
-    cout << "Thread 2 is running. ID: " << this_thread::get_id() << "\n";
-}
-
-int main() {
-    thread t1(task1);
-    thread t2(task2);
-
-    // Get thread IDs
-    cout << "t1 ID: " << t1.get_id() << "\n";
-    cout << "t2 ID: " << t2.get_id() << "\n";
-
-    // Join t1 if joinable
-    if (t1.joinable()) {
-        t1.join();
-        cout << "t1 joined\n";
-    }
-
-    // Detach t2
-    if (t2.joinable()) {
-        t2.detach();
-        cout << "t2 detached\n";
-    }
-
-    cout << "Main thread sleeping for 1 second...\n";
-    this_thread::sleep_for(chrono::seconds(1));
-    cout << "Main thread awake.\n";
-
-    return 0;
+int main () 
+{ 
+    // value of res is computed at compile time. 
+    constexpr long int res = fib(30);
+    std::cout << res; 
+    return 0; 
 }
